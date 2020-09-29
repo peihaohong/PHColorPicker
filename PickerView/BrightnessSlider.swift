79e6d3d94d5ebec136c67d46efb7d91da304989a
peihaohong <402933963@qq.com>
2020-09-29 20:26:26 +0800

    设置亮度滑动

---
+++ PickerView/BrightnessSlider.swift
@@ -0,0 +1,57 @@
+//
+//  BrightnessSlider.swift
+//  PHColorPicker
+//
+//  Created by hph on 2020/9/29.
+//  Copyright © 2020 hph. All rights reserved.
+//
+
+import UIKit
+
+class BrightnessSlider: RootSlider {
+    
+    var gradientLayer = CAGradientLayer.init()
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func awakeFromNib() {
+         super.awakeFromNib()
+               
+    }
+   
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.setupLayer()
+    }
+    
+    func setupLayer(){
+        gradientLayer = CAGradientLayer.init()
+        gradientLayer.bounds = bounds
+        gradientLayer.position = CGPoint.init(x:frame.size.width * 0.5, y: frame.size.height * 0.5)
+        gradientLayer.startPoint = CGPoint.init(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint.init(x: 1, y: 0.5)
+        gradientLayer.cornerRadius = 8.0
+        gradientLayer.borderWidth = 2
+        gradientLayer.masksToBounds = true
+        gradientLayer.borderColor = UIColor.red.cgColor
+        
+        if (self.responds(to: #selector(getter: contentScaleFactor))){
+            self.contentScaleFactor = UIScreen.main.scale
+        }
+        
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        setKeyColor(UIColor.blue)
+        
+    }
+    
+    func setKeyColor(_ color: UIColor)
+    {
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        gradientLayer.colors = [color.cgColor,UIColor.black.cgColor]
+        CATransaction.commit()
+    }
+
+}
--- PickerView/RootSlider.swift
+++ PickerView/RootSlider.swift
@@ -9,9 +9,81 @@
 import UIKit
 
 class RootSlider: UIControl {
-    var sliderKnobView :UIImageView?
+    var sliderAimingView :UIImageView?
+    var  isHorizontal = true
+    var valueSlider : CGFloat = 0.0
+    let ainmingWidth : CGFloat = 40.0
     
-    var  isHorizontal : Bool?
-    var value = 0.0;
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        //        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        setupSubViews()
+    }
+    
+    func setupSubViews()
+    {
+        let frame = self.frame
+        isHorizontal = frame.size.width > frame.size.height
+        
+        sliderAimingView = UIImageView.init(image: UIImage.init(named: "targetLight"))
+        self.addSubview(sliderAimingView!)
+//        self.backgroundColor = UIColor.blue
+        self.isUserInteractionEnabled = true
+        self.valueSlider = 0.0
+    }
+    
+    func setValue(_ valueInput: CGFloat) { 
+        //若是水平 则横坐标为视图 视图的宽*value
+        //若是垂直 则横坐标为视图二分一
+        let x = isHorizontal ? frame.size.width * valueSlider : frame.size.width / 2
+        let y = isHorizontal ? frame.size.height / 2 : frame.size.width * valueSlider
+        sliderAimingView?.center = CGPoint.init(x: x, y: y)
+    }
+    
+    
+    func mapPointToValue(_ point : CGPoint)
+    {
+        valueSlider = isHorizontal ? point.x / frame.size.width : point.y / frame.size.height
+        setValue(valueSlider)
+        self.sendActions(for: .valueChanged)
+    }
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        
+        let location = touch.location(in: self)
+        let   canMove = isHorizontal ? location.x < frame.size.width && location.x >= 0 : location.y  < frame.size.height && location.y >= 0
+        
+        if canMove {
+            self.mapPointToValue(touch.location(in: self))
+            return true
+        }else{
+            return false
+        }
+        
+    }
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let location = touch.location(in: self)
+        let canMove = isHorizontal ? location.x < frame.size.width && location.x >= 0 : location.y  < frame.size.height && location.y >= 0
+        
+        if canMove {
+            self.mapPointToValue(touch.location(in: self))
+            return true
+        }else{
+            return false
+        }
+        
+        
+        
+    }
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        self.continueTracking(touch!, with: event)
+    }
+    override func didAddSubview(_ subview: UIView) {
+        self.bringSubviewToFront(sliderAimingView!)
+    }
 }
