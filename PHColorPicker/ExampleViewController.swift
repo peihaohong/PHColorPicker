8d14c2da7026dca4e2bedb038fd378d6fba36b65
peihaohong <402933963@qq.com>
2020-09-29 13:21:30 +0800

    增加一些东西

---
+++ PHColorPicker/ExampleViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ExampleViewController.swift
+//  PHColorPicker
+//
+//  Created by hph on 2020/9/29.
+//  Copyright © 2020 hph. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        UIRectEdgeNone
+        self.edgesForExtendedLayout = UIRectEdge.bottom
+        self.navigationItem.title = "取色器示例"
+    }
+
+
+   
+
+}
