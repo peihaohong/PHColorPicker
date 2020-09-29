e603ddd8411e3359d6dadd53bf72b6c466495275
peihaohong <402933963@qq.com>
2020-09-29 17:06:14 +0800

    sf

---
+++ PickerView/HSWheelControl.swift
@@ -0,0 +1,21 @@
+//
+//  HSWheelControl.swift
+//  PHColorPicker
+//
+//  Created by hph on 2020/9/29.
+//  Copyright © 2020 hph. All rights reserved.
+//
+
+import UIKit
+
+class HSWheelControl: UIControl {
+    
+    var ivWheel : UIImageView?
+    var ivAiming : UIImageView?
+     
+    required init?(coder: NSCoder){
+        super.init(coder: coder)
+    }
+    
+
+}
---
+++ PickerView/RootSlider.swift
@@ -0,0 +1,17 @@
+//
+//  RootSlider.swift
+//  PHColorPicker
+//
+//  Created by hph on 2020/9/29.
+//  Copyright © 2020 hph. All rights reserved.
+//
+
+import UIKit
+
+class RootSlider: UIControl {
+    var sliderKnobView :UIImageView?
+    
+    var  isHorizontal : Bool?
+    var value = 0.0;
+
+}
